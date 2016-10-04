7e04290753c746ed35a95816154f671d9b1747c4
Ondieki <mondieki9@gmail.com>
2016-10-04 08:11:36 +0300

    Indentation and line spacing.

--- BresenhamLine.py
+++ BresenhamLine.py
@@ -7,12 +7,12 @@ class bresenham:
 		self.steep = abs(self.end[1]-self.start[1]) > abs(self.end[0]-self.start[0])
 
 		if self.steep:
-			print ('Steep Slope')
+			print ('Steep gradient')
 			self.start = self.swap(self.start[0],self.start[1])
 			self.end = self.swap(self.end[0],self.end[1])
 
 		if self.start[0] > self.end[0]:
-			print ('Shallow Slope')
+			print ('Shallow gradient')
 			_x0 = int(self.start[0])
 			_x1 = int(self.end[0])
 			self.start[0] = _x1
@@ -23,10 +23,10 @@ class bresenham:
 			self.start[1] = _y1
 			self.end[1] = _y0
 
-		dx = self.end[0] - self.start[0]
-		dy = abs(self.end[1] - self.start[1])
+		dx = self.end[0] - self.start[0] #change in x
+		dy = abs(self.end[1] - self.start[1]) #change in y
 		error = 0
-		derr = dy/float(dx)
+		derr = dy/float(dx) #gradient
 
 		ystep = 0
 		y = self.start[1]
@@ -55,14 +55,16 @@ class bresenham:
 	def swap(self,n1,n2):
 		return [n2,n1]
 
-print("Consider drawing a line on a raster grid where we restrict the allowable slopes of the line to the range: 0 < m <= 1")
+print("Consider drawing a line on a raster grid where we restrict the allowable slopes of the line to the range: 0 < m <= 1 \n\n")
 print ("Starting coordinates: ")
 x1=int(input("x: "))
 y1=int(input("y: "))
 print ("Ending coordinates: ")
 x2=int(input("x: "))
 y2=int(input("y: "))
+print ("\n")
 l = bresenham([x1,y1],[x2,y2])
+print ("\nResult: ")
 print (l.path)
 
 
